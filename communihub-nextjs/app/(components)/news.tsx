b0a9f495865f4581797e4cf1c63fa139c7076c40
armameje <armamentojenisi@gmail.com>
2023-05-17 05:36:31 +0800

    disable warning

--- communihub-nextjs/app/(components)/news.tsx
+++ communihub-nextjs/app/(components)/news.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function News() {
   return (
     <section className="w-fill h-auto bg-[rgb(250,236,225)] flex flex-col justify-center items-center gap-4 py-16">
@@ -9,9 +11,9 @@ export default function News() {
       <button className="border-2 px-8 py-2 bg-green-950 text-neutral-50 text-xs hover:text-green-950 hover:bg-white hover:border-green-950 hover:transition-all">Start Now</button>
       <div className="relative">
           <div className="w-[800px] h-[450px] flex justify-center items-center news bg-cover bg-center"></div>
-          {/* <Link href="/services" className="bg-green-950 text-white py-2 px-4 hover:bg-white hover:text-green-950 transition-all z-10 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
+          <Link href="/services" className="bg-green-950 text-white py-2 px-4 hover:bg-white hover:text-green-950 transition-all z-10 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
             Services
-          </Link> */}
+          </Link>
       </div>
     </section>
   );
