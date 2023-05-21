8e59b199fcf79e18c9b824e1dc645e0bd0c8c5c0
larrjoshua <132808713+larrjoshua@users.noreply.github.com>
2023-05-21 23:54:05 +0800

    UpdateAbout

--- communihub-nextjs/app/(components)/navbar.tsx
+++ communihub-nextjs/app/(components)/navbar.tsx
@@ -1,23 +1,14 @@
-"use client";
-
-import { useSession } from "next-auth/react";
-import { signOut } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { FaFacebookF, FaPinterestP, FaTwitter } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
 import Link from "next/link";
 
 export default function NavBar() {
-  const session = useSession();
-  const { push } = useRouter();
-
-  function handleSignOut() {
-    signOut({ redirect: false, callbackUrl: "/auth/login" });
-    push("/auth/login");
-  }
 
   return (
-    <nav className="w-full flex justify-between border-2 items-center border-stone-800 px-10 py-1 sticky top-0 z-10 bg-[rgba(255,99,71,0.2)]">
-      <div className="relative w-64 h-16 overflow-visible">
-        <img src="/new_logo.png" alt="communihub" className="absolute top-[-135%]" />
+    <nav className="w-full flex justify-between border-2 items-center border-stone-800 px-16 py-6 sticky">
+      <div className="flex items-center relative">
+        <div className="logo w-[150px] h-[120px] bg-cover absolute"></div>
+        <input type="text" placeholder="Search..." className="ml-44 w-16" />
       </div>
       <div className="flex justify-around gap-8 ">
         <Link href="/" className="hover:text-[#015243] font-bold">
@@ -36,7 +27,25 @@ export default function NavBar() {
           About Us
         </Link>
       </div>
-      <div className="flex gap-8 w-[205px]">{session.status === "authenticated" ? <button onClick={handleSignOut}>Sign Out</button> : <Link href="/auth/login">Log In</Link>}</div>
+      <div className="flex gap-8 w-[205px]">
+        {/* <div className="flex items-center gap-4">
+          <span>
+            <FaFacebookF />
+          </span>
+          <span>
+            <BsInstagram />
+          </span>
+          <span>
+            <FaPinterestP />
+          </span>
+          <span>
+            <FaTwitter />
+          </span>
+        </div>
+        <a href="#" className="border-2 border-stone-950 rounded-full p-2">
+          Login
+        </a> */}
+      </div>
     </nav>
   );
 }
--- communihub-nextjs/app/about/page.tsx
+++ communihub-nextjs/app/about/page.tsx
@@ -1,45 +1,48 @@
-import Footer from "../(components)/footer";
+import { getServerSession } from "next-auth/next";
+import { redirect } from "next/navigation";
 
 export const metadata = {
   title: "About Communihub",
 };
 
 export default async function AboutUs() {
+  // const session = await getServerSession(authOptions);
+
+  // if (!session) {
+  //   redirect("/auth/login?callbackUrl=/");
+  // }
 
   return (
-    <>
-      <section>
-        <div className="w-full h-[980px] flex justify-center poster-bg">
-          <img src="/aboutUsPoster.png" alt="test" />
-        </div>
-        <div className="flex flex-col px-60 py-16 gap-8 about-content-header">
-          <h1 className="text-[86px]">About Us</h1>
-          <p className="text-[25px]">
-            CommuniHub is a software platform designed to bring communities together by streamlining essential functions and encouraging interactions between residents. Our user-friendly platform
-            features a comprehensive community map, entry and exit detection system for added security, an online market for local businesses, and group messaging for residents to work together on
-            projects and events. Our goal is to promote community building and support local businesses, ultimately improving the quality of life for residents.
+    <section>
+      <div className="w-full h-[980px] flex justify-center poster-bg">
+        <img src="/aboutUsPoster.png" alt="test" />
+      </div>
+      <div className="flex flex-col px-60 py-10 gap-1 about-content-header">
+        <h1 className="text-[86px] font-bold">About Us</h1>
+        <p className="text-[20px]">
+          CommuniHub is a software platform designed to bring communities together by streamlining essential functions and encouraging interactions between residents. Our user-friendly platform
+          features a comprehensive community map, entry and exit detection system for added security, an online market for local businesses, and group messaging for residents to work together on
+          projects and events. Our goal is to promote community building and support local businesses, ultimately improving the quality of life for residents.
+        </p>
+      </div>
+      <div className="flex">
+        <div className="flex flex-col items-center justify-top px-5 py-10 bg-[rgb(29,14,3)] basis-1/2">
+          <h1 className="text-[50px] text-[rgb(250,236,225)] text-right w-full font-bold italic">Our Mission</h1>
+          <p className="text-lg text-[rgb(250,236,225)] text-right text-[18px] ">
+            Our mission is to create a stronger sense of community within residential areas by providing an all-in-one platform that streamlines essential community functions and fosters a culture of
+            togetherness and engagement. We strive to enhance security, promote local businesses, and empower residents to connect, share resources, and work together on projects that benefit the
+            community as a whole.
           </p>
         </div>
-        <div className="flex">
-          <div className="flex flex-col items-center justify-center px-8 py-16 bg-[rgb(29,14,3)] basis-1/2">
-            <h1 className="text-[56px] text-[rgb(250,236,225)] text-right w-full">Our Mission</h1>
-            <p className="text-lg text-[rgb(250,236,225)] text-right">
-              Our mission is to create a stronger sense of community within residential areas by providing an all-in-one platform that streamlines essential community functions and fosters a culture
-              of togetherness and engagement. We strive to enhance security, promote local businesses, and empower residents to connect, share resources, and work together on projects that benefit the
-              community as a whole.
-            </p>
-          </div>
-          <div className="flex flex-col items-start justify-center px-8 py-16 bg-[rgb(250,236,225)] basis-1/2">
-            <h1 className="text-[56px]">Our Vision</h1>
-            <p className="text-lg">
-              Our vision is to transform residential communities into vibrant, connected, and prosperous neighborhoods where residents feel a strong sense of belonging and pride. We aim to achieve
-              this by providing an innovative software platform that enhances the quality of life for residents, supports local businesses, and fosters a culture of cooperation and engagement. By
-              creating strong and healthy neighborhoods, we believe that we can make a positive impact on society as a whole, creating a better future for all.
-            </p>
-          </div>
+        <div className="flex flex-col items-start justify-center px-5 py-10 bg-[rgb(250,236,225)] basis-1/2">
+          <h1 className="text-[50px] font-bold italic">Our Vision</h1>
+          <p className="text-lg text-[18px]">
+            Our vision is to transform residential communities into vibrant, connected, and prosperous neighborhoods where residents feel a strong sense of belonging and pride. We aim to achieve this
+            by providing an innovative software platform that enhances the quality of life for residents, supports local businesses, and fosters a culture of cooperation and engagement. By creating
+            strong and healthy neighborhoods, we believe that we can make a positive impact on society as a whole, creating a better future for all.
+          </p>
         </div>
-      </section>
-      <Footer />
-    </>
+      </div>
+    </section>
   );
 }
