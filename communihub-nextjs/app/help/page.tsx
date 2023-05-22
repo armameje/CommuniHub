801e0a637f9ae31b95c0b133915e19cab6952206
larrjoshua <132808713+larrjoshua@users.noreply.github.com>
2023-05-22 12:54:06 +0800

    Update page.tsx

--- communihub-nextjs/app/help/page.tsx
+++ communihub-nextjs/app/help/page.tsx
@@ -10,9 +10,9 @@ export default async function Help() {
 
   return (
     <><section>
-      <div className="flex flex-col item-center justify-center gap-10 bg-[rgb(250,236,225)] pb-16">
-        <div className="flex justify-center items-center mt-16 mb-6">
-          <h1 className="text-6xl font-bold">Contact Us</h1>
+      <div className="flex flex-col item-center justify-center gap-5 bg-[rgb(250,236,225)] pb-10">
+        <div className="flex justify-center items-center mt-16 mb-3">
+          <h1 className="text-6xl font-bold mb-1">Contact Us</h1>
         </div>
         <div className="flex justify-between items-start pl-[300px] gap-8">
           <div className="basis-1/4 pr-4 flex flex-col gap-4 items-start">
@@ -55,22 +55,22 @@ export default async function Help() {
       </div>
 
       <div className="flex flex-col items-center px-35 py-18 bg-[rgb(221,190,166)]">
-      <h1 className="text-5xl mb-8"></h1>
-  <div className="flex flex-col items-center gap-30 mt-30 px-32">
-    <h1 className="text-5xl font-bold mb-8">Talk to real people</h1>
-    <p className="text-center">
-      CommuniHub's exceptional customer support, always ready to help creators navigate the complexities of community building, is a primary reason they choose us. Our dedicated team, available 24/7, assists with everything from basic setup to advanced growth strategies, ensuring that you can focus on creating content and fostering connections. Always striving for improvement, we value your feedback and understand that our success is tied to yours. At CommuniHub, we don't just provide a service, but build enduring relationships, standing with you every step of your journey.
-      <h1 className="text-5xl mb-8"></h1>
-    </p>
-    <div>
-  </div>
-</div>
+        <h1 className="text-5xl mb-8"></h1>
+        <div className="flex flex-col items-center gap-30 mt-30 px-32">
+          <h1 className="text-5xl font-bold mb-8">Talk to real people</h1>
+          <p className="text-center">
+            CommuniHub's exceptional customer support, always ready to help creators navigate the complexities of community building, is a primary reason they choose us. Our dedicated team, available 24/7, assists with everything from basic setup to advanced growth strategies, ensuring that you can focus on creating content and fostering connections. Always striving for improvement, we value your feedback and understand that our success is tied to yours. At CommuniHub, we don't just provide a service, but build enduring relationships, standing with you every step of your journey.
+            <h1 className="text-5xl mb-8"></h1>
+          </p>
+          <div>
+          </div>
+        </div>
         <div className="w-full flex flex-wrap justify-center gap-1">
           {memberList.map(({ key, name, location, id }) => {
             return <MemberBox key={key} name={name} location={location} id={id} />;
           })}
         </div>
       </div>
-    </section><Footer /></>  
-  );  
+    </section><Footer /></>
+    );  
 }
