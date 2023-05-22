682ab3e2ecb144cd8613b5f1cc688d41862e345e
larrjoshua <132808713+larrjoshua@users.noreply.github.com>
2023-05-22 15:36:35 +0800

    LarrRevisions

--- communihub-nextjs/app/market/(components)/menu-item.tsx
+++ communihub-nextjs/app/market/(components)/menu-item.tsx
@@ -2,12 +2,12 @@ import { menuItem } from "@/public/interface/iMenuItem";
 
 export default function MenuItem({ name, price, itemCode }: menuItem) {
   return (
-    <div className="w-full flex mb-8 gap-16">
+    <div className="w-full flex mb-1 gap-10">
       <div>
         <img src={`/${itemCode}.jpg`} alt={`name`} width={400} height={200} />
       </div>
       <div className="flex flex-col items-center justify-center grow">
-        <div className="text-xl">Menu Title: <span className="text-2xl font-bold">{name}</span></div>
+        <div className="text-xl">Specialty: <span className="text-2xl font-bold">{name}</span></div>
         <div className="text-xl">&#x20B1; {price}</div>
       </div>
     </div>
