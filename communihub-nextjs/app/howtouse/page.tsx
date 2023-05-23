8695dd5e46ea00a69a19ad39277339e3628213d1
ArmamentoNikko <armamentonikkonel@gmail.com>
2023-05-24 07:42:28 +0800

    fixedHowtouse

---
+++ communihub-nextjs/app/howtouse/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Footer from "../(components)/footer";
+
+export default function Announcement() {
+
+  return (
+    <><section className="w-auto h-auto bg-[rgb(250,236,225)] flex flex-col justify-center items-center px-48 py-20 gap-8">
+    <h1 className="text-6xl font-bold  text-black ">How To Use Communihub?</h1>
+    <p>Welcome to CommuniHub! Here's a user-friendly guide to help you navigate and make the most of our platform.</p>
+    <h1 className="text-2xl font-bold  text-black"> Log In</h1>
+    <p>To access CommuniHub, you need to log in. If you're a registered user, enter your username and password.</p>
+    <h1 className="text-2xl font-bold  text-black"> Home Page</h1>
+    <p>Once logged in, you'll land on our Home page. This page provides a snapshot of the latest happenings, news, and announcements. Here, you'll find a brief introduction to CommuniHub and an overview of what we offer. You can access by clicking the buttons  different services, participate in various community activities, and support local businesses all from this page.</p>
+    <h1 className="text-2xl font-bold  text-black"> Footer</h1>
+    <p>Scroll down to the bottom of any page and you'll find our footer. Here, you can access our social media links (Facebook, Instagram, Pinterest, and Twitter) to stay connected and updated.</p>
+    <h1 className="text-2xl font-bold  text-black ">Services</h1>
+    <p>In the Services section, you can find a variety of offerings including cleaning, gardening, plumbing, electrician, fashion, and transportation services​. Our platform connects you with skilled professionals who specialize in various services, ensuring you receive quality assistance for your home-related needs​​. You can also explore the community through our interactive map feature​.</p>
+    <h1 className="text-2xl font-bold  text-black"> Market</h1>
+    <p>Visit the Market page to experience the vibrant blend of flavors and community at Villas Del Pueblo Market. Discover a rich tapestry of tastes and shared connections, and immerse yourself in the diverse array of offerings from local vendors​</p>
+    <h1 className="text-2xl font-bold  text-black"> Help Page</h1>
+    <p>The Help page provides our contact details, including our address, contact number, email, and operating hours. If you need assistance, you can reach out to us, and our dedicated team will assist you with everything from basic setup to advanced growth strategies​​​. This page also includes the contact details of specific community members​​.</p>
+    <h1 className="text-2xl font-bold  text-black"> About Us</h1>
+    <p>Visit the About Us page to learn more about CommuniHub. Our mission is to create a stronger sense of community within residential areas by providing an all-in-one platform that streamlines essential community functions and fosters a culture of togetherness and engagement​​. Our vision is to transform residential communities into vibrant, connected, and prosperous neighborhoods, creating a better future for all residents​​.
+
+Remember, at CommuniHub, we're all about community. We're here to help you connect, share resources, and work together. Don't hesitate to reach out if you need any assistance navigating our platform. Happy community building!
+</p>
+
+
+  </section>
+  <Footer></Footer></>
+  );
+}
