a83a3440a88e666062af964a44e0cc30325476d1
armameje <armamentojenisi@gmail.com>
2023-05-24 23:23:57 +0800

    Add login validation and video

--- communihub-nextjs/app/auth/login/page.tsx
+++ communihub-nextjs/app/auth/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
-import { motion } from "framer-motion";
+import { animate, motion } from "framer-motion";
 import { FormEvent, FormEventHandler, useState } from "react";
 
 export default function LoginPage() {
@@ -10,6 +10,7 @@ export default function LoginPage() {
   // console.log(searchParams);
   // const callbackUrl: any = searchParams.get("callbackUrl");
   const [userInfo, setUserInfo] = useState({ username: "", password: "" });
+  const [showValidation, setShowValidation] = useState(false);
   const { push } = useRouter();
 
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
@@ -22,7 +23,15 @@ export default function LoginPage() {
     });
 
     console.log(res);
-    if (res?.error === null) push("/");
+    if (res?.error === null) {
+      push("/");
+    } else {
+      setShowValidation(true);
+
+      setTimeout(() => {
+        setShowValidation(false);
+      }, 2000);
+    }
     setUserInfo({ username: "", password: "" });
   }
 
@@ -34,9 +43,17 @@ export default function LoginPage() {
           <div className="flex flex-col items-center justify-center mb-4 leading-none">
             <h1 className="text-3xl">Welcome to Communihub!</h1>
           </div>
-          <form className="flex flex-col justify-center items-center grow" onSubmit={handleSubmit}>
+          <form className="flex flex-col justify-center items-center grow relative" onSubmit={handleSubmit}>
             <div className="relative mb-5">
-              <input type="text" className="form-input" name="username" value={userInfo.username} onChange={(e) => setUserInfo({ ...userInfo, username: e.target.value })} required />
+              <input
+                type="text"
+                className="form-input"
+                name="username"
+                autoComplete="off"
+                value={userInfo.username}
+                onChange={(e) => setUserInfo({ ...userInfo, username: e.target.value })}
+                required
+              />
               <label className="form-label">Username</label>
             </div>
             <div className="relative mb-8">
@@ -44,9 +61,13 @@ export default function LoginPage() {
               <label className="form-label">Password</label>
             </div>
             <button>Login</button>
+            {showValidation && (
+              <motion.div initial={{opacity: 0}} animate={{opacity:1}} className="w-52 h-16 bg-red-200 absolute top-1/3 left-1/2 translate-x-[-50%] translate-y-[-50%] flex justify-center items-center text-center rounded">
+                <p className="text-red-700">Invalid username or password</p>
+              </motion.div>
+            )}
           </form>
         </div>
-        <div></div>
       </motion.div>
     </section>
   );
